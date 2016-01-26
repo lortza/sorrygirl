dfe78c3ab2787e87bd57c1b0cfdd0f458f6440dd
anne <richardson.ae@gmail.com>
2016-01-26 14:48:31 -0600

    user before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # before_action :require_signin
+  before_action :require_signin, except: [:new, :create]
 
 
   # GET /users
