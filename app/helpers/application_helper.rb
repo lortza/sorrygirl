32d4214ce250f2c1c9fd7b12347382c200a8af95
anne <richardson.ae@gmail.com>
2016-01-26 11:33:18 -0600

    page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
-end
+ def page_title
+   if content_for?(:title)
+     "Sorry Girl - #{content_for(:title)}"
+    else
+     "Sorry Girl - The Apology You Needed to Hear, but from Ryan Gosling"
+   end #content_for
+ end #page_title
+end #ApplicationHelper
