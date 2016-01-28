cd88e9bbb7e9438f0f57c6966a953a3b4b3924bc
anne <richardson.ae@gmail.com>
2016-01-27 22:20:22 -0600

    validations, scope, before actions

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,11 @@ class Page < ActiveRecord::Base
   validates :name, :body, presence: true
   validates :slug, uniqueness: { case_sensitive: false }
 
+  PUBLISH_STATUS = %w(Published Unpublished)
+  validates :publish_status, inclusion: { in: PUBLISH_STATUS }
+
+  scope :published, -> { where(:publish_status => 'Published').order("name") }
+
 
   def generate_slug
     self.slug = name.downcase 
