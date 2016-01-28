3055526c679b5ae178a6f0ce3d7bbfa5e5f51443
anne <richardson.ae@gmail.com>
2016-01-28 00:39:33 -0600

    added Home and About seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Page.create!([
+  {
+    name: 'About',
+    heading: "About this App",
+    body: "This is where your content goes",
+    slug: 'about',
+    publish_state: "Published" 
+
+  },
+  {
+    name: 'static-home-page',
+    heading: "Go to the Root Page",
+    body: "Home page content goes here. This page will look weird as 'pages/ static-home-page' so be sure to click on the root to see the actual page",
+    slug: 'static-home-page',
+    publish_state: "Published" 
+
+  }
+])
