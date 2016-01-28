f18388a1e11e4173679fec3f4f03e0a17f84f80e
anne <richardson.ae@gmail.com>
2016-01-27 22:20:35 -0600

    button and style updates for views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127224649) do
+ActiveRecord::Schema.define(version: 20160128031131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20160127224649) do
     t.string   "slug"
     t.string   "heading"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "publish_status", default: "Published"
   end
 
   create_table "users", force: :cascade do |t|
