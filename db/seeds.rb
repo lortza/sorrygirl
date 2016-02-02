b59f9e5e699e9e6ccdb1c7ce328b13ed009cc52d
truongmkevin <truongmkevin@yahoo.com>
2016-02-02 07:29:08 +0000

    Corrected name from publish_state to publish_status

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Page.create!([
     heading: "About this App",
     body: "This is where your content goes",
     slug: 'about',
-    publish_state: "Published" 
+    publish_status: "Published"
 
   },
   {
@@ -20,7 +20,7 @@ Page.create!([
     heading: "Go to the Root Page",
     body: "Home page content goes here. This page will look weird as 'pages/ static-home-page' so be sure to click on the root to see the actual page",
     slug: 'static-home-page',
-    publish_state: "Published" 
+    publish_status: "Published"
 
   }
 ])
