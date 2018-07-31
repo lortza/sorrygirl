e4df25c8eb7fbf5df1f7679febf34c9e8ab3ddb8
anne <richardson.ae@gmail.com>
2018-07-31 18:05:23 -0500

    Renames method for clarity

--- app/models/apology.rb
+++ app/models/apology.rb
@@ -1,12 +1,12 @@
 class Apology < ActiveRecord::Base
 
-  before_save :generate_image
+  before_create :assign_image
 
   validates :body, presence: true, length: { maximum: 170 }
 
   scope :ordered, -> { all.order("created_at DESC") }
 
-  def generate_image
+  def assign_image
     self.image = Image.select
   end
 
