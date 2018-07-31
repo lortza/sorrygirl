d27faae3b089f94676559e5e9ceb10518e27d4c3
anne <richardson.ae@gmail.com>
2018-07-30 21:40:30 -0500

    Displays remaining character count for form

---
+++ app/assets/javascripts/apologies.js
@@ -0,0 +1,25 @@
+document.addEventListener('turbolinks:load', function () {
+  let textBox = document.querySelector('.form-box textarea')
+  let formInputArea = document.querySelector('#apology-content')
+  let characterMax = 220
+  let submitButton = document.querySelector('input[type=submit]')
+
+  let displayRemainingCharacters = function(){
+    let currentCount = textBox.value.length
+    let remainingCharacters = characterMax - currentCount
+
+    document.querySelector('#character-count').textContent = remainingCharacters
+
+    if (currentCount <= characterMax) {
+      formInputArea.classList.remove('exceeded-limit')
+      submitButton.disabled = false
+      submitButton.classList.remove('btn-secondary')
+    } else {
+      formInputArea.classList.add('exceeded-limit')
+      submitButton.disabled = true
+      submitButton.classList.add('btn-secondary')
+
+    }
+  }
+  document.addEventListener('keyup', displayRemainingCharacters)
+});
