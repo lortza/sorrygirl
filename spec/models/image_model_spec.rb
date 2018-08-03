b5c7dc84ebb72686dbd6d0751977670438172520
anne <richardson.ae@gmail.com>
2018-08-03 12:34:27 -0500

    Tests image_file_names

--- spec/models/image_model_spec.rb
+++ spec/models/image_model_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Image, :type => :model do
       expect(Image.sample).to eq('image.jpg')
     end
   end
+
+  describe '.image_file_names' do
+    it 'returns an array of image file basenames without the path' do
+      filepaths = ['/ipsum/image.jpg', '/ipsum/image.pdf']
+      allow(Image).to receive(:filepaths).and_return(filepaths)
+
+      expect(Image.image_file_names.any?('/ipsum/')).to be false
+      expect(Image.image_file_names.first).to eq('image.jpg')
+    end
+  end
 end
