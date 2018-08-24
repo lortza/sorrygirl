8a4d2234f1fc587d500b23c016bd0bf605281592
anne <richardson.ae@gmail.com>
2018-08-24 16:24:46 -0500

    Extracts responses into methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,31 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-       session[:user_id] = user.id
-       flash[:notice] = "Welcome back, #{user.first_name}!"
-       redirect_to(session[:intended_url] || user)
-       session[:intended_url] = nil
-     else
-      flash.now[:alert] = "Invalid email/password combination!"
-      render :new
     user = GuaranteedUser.find_by(email: params[:email])
 
     if user.authenticate(params[:password])
+      create_user_session(user)
+    else
+      display_error(user)
     end
   end
 
-   def destroy
-     session[:user_id] = nil 
-     redirect_to root_url, notice: "You're now signed out!"
-     
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "You're now signed out!"
   end
+
+  private
+
+  def create_user_session(user)
+    session[:user_id] = user.id
+    flash[:notice] = "Welcome back, #{user.first_name}!"
+    redirect_to(session[:intended_url] || user)
+    session[:intended_url] = nil
+  end
+
+  def display_error
+    flash.now[:alert] = 'Invalid email/password combination!'
+    render :new
+  end
+end
