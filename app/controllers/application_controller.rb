0a6b3ba931e233ede630382bbd589bab026f961d
anne <richardson.ae@gmail.com>
2018-08-24 16:17:49 -0500

    Converts conditions to guard clauses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
 
   before_action :set_pages
 
-private
+  private
 
   def require_signin
-    unless current_user
-      session[:intended_url] = request.url
-      redirect_to new_session_url, alert: "Please sign in first"
+    return nil if current_user
+    session[:intended_url] = request.url
+    redirect_to new_session_url, alert: 'Please sign in first'
   end
 
   def current_user
